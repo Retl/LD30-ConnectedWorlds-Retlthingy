b3c1c06e00dc57ee590646206135016d5d8606bb
Retl <carlis.moore@gmail.com>
2014-08-22 23:20:55 -0400

    Two playable characters
    Added playable characters Blue and Yellow. Yellow can fly freely, blue
    is more platformery. Still need to figure out how to separate their
    world thingies or something.  And add a camera switch button. And

---
+++ Assets/LD30/Scripts/PlayerScripts/PlayerController_Blue.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController_Blue : MonoBehaviour {
+
+	public AudioSource jumpingSound;
+	public AudioSource landingSound;
+	public AudioSource attackingSound;
+
+	public float vert = 0;
+	public float horiz = 0;
+	public bool doJump = false;
+	public bool doAct = false;
+
+	float movespeed = 128.0f;
+	float jumpSpeed = 9.0f;
+
+	float dt = 0;
+	float attackCooldown = 1.5f;
+	float attackCooldownTimer = 0.0f;
+
+	bool selected = false;
+	int canJump = 1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Get the values from the input so we can use it soon.
+		dt = Time.deltaTime;
+		horiz = Input.GetAxis("Horizontal");
+		vert = Input.GetAxis("Vertical");
+		doJump = Input.GetButtonDown("Fire1");
+		doAct = Input.GetButtonDown("Fire2");
+
+		//Update any countdown timers before trying to do things that depend on them.
+		if (attackCooldownTimer > 0)
+		{
+			attackCooldownTimer -= dt;
+		}
+
+		//Move around, jump, attaack, etc. Basically make use of the input values now.
+		if (rigidbody2D != null)
+		{
+			rigidbody2D.velocity = new Vector2(horiz * movespeed * dt, rigidbody2D.velocity.y);
+		}
+
+		if (doJump && canJump > 0)
+		{
+			rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpSpeed);
+		}
+	
+	}
+}
---
+++ Assets/LD30/Scripts/PlayerScripts/PlayerController_Yellow.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController_Yellow : MonoBehaviour {
+
+	public AudioSource jumpingSound;
+	public AudioSource landingSound;
+	public AudioSource attackingSound;
+	
+	public float vert = 0;
+	public float horiz = 0;
+	public bool doAct = false;
+	
+	float movespeed = 128.0f;
+	
+	float dt = 0;
+	float attackCooldown = 1.5f;
+	float attackCooldownTimer = 0.0f;
+	
+	bool selected = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Get the values from the input so we can use it soon.
+		dt = Time.deltaTime;
+		horiz = Input.GetAxis("Horizontal");
+		vert = Input.GetAxis("Vertical");
+		doAct = Input.GetButtonDown("Fire2");
+		
+		//Update any countdown timers before trying to do things that depend on them.
+		if (attackCooldownTimer > 0)
+		{
+			attackCooldownTimer -= dt;
+		}
+		
+		//Move around, jump, attaack, etc. Basically make use of the input values now.
+		if (rigidbody2D != null)
+		{
+			rigidbody2D.velocity = new Vector2(horiz * movespeed * dt, vert * movespeed * dt);
+		}
+		
+	}
+}
